60462f94ed03aa43f49aa8f1cf10edc153cd0df9
canofbubly <derekrussellbroden@gmail.com>
2025-06-05 19:54:56 -0500

    Troubleshooting

---
+++ app.js
@@ -0,0 +1,11 @@
+// app.js
+const express = require("express");
+const app = express();
+const usersRouter = require("./routes/usersRouter");
+
+app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: true }));
+app.use("/", usersRouter);
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Your express app is up! http://localhost:${PORT}!`));
